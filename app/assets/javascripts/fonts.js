841bf39adab17302da925bcd4dd144ce6ff53412
Zaid Huda <tripledot99@gmail.com>
2017-04-18 22:17:41 +0800

    improve js loading

--- app/assets/javascripts/fonts.js
+++ app/assets/javascripts/fonts.js
@@ -1,2 +1,9 @@
-$('head').append('<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">');
-$('head').append('<link href="https://fonts.googleapis.com/css?family=Roboto+Slab|Roboto:400,700" rel="stylesheet">');
+var link=document.createElement('link');
+link.rel='stylesheet';
+link.href="https://fonts.googleapis.com/icon?family=Material+Icons";
+document.getElementsByTagName('head')[0].appendChild(link);
+
+link=document.createElement('link');
+link.rel='stylesheet';
+link.href="https://fonts.googleapis.com/css?family=Roboto+Slab|Roboto:400,700";
+document.getElementsByTagName('head')[0].appendChild(link);
