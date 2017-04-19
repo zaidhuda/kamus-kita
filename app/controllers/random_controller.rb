e69e9c13f8881c6edefc3cfecd825b9f5174fa4a
Zaid Huda <tripledot99@gmail.com>
2017-04-19 18:48:29 +0800

    turbolinks random and hide dropdown by default

--- app/controllers/random_controller.rb
+++ app/controllers/random_controller.rb
@@ -1,6 +1,6 @@
 class RandomController < ApplicationController
   def index
     random_word = Word.offset(rand(Word.count)).first
-    redirect_to word_path(random_word)
+    redirect_to word_path(random_word), turbolinks: true
   end
 end
