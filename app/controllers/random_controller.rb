addc6f81fb17ce7dd73356c82035dd1e1ae5b4e7
Zaid Huda <tripledot99@gmail.com>
2017-04-19 18:58:12 +0800

    updated random function

--- app/controllers/random_controller.rb
+++ app/controllers/random_controller.rb
@@ -1,6 +1,11 @@
 class RandomController < ApplicationController
   def index
-    random_word = Word.offset(rand(Word.count)).first
-    redirect_to word_path(random_word), turbolinks: true
+    @word = Word.offset(rand(Word.count)).first
+    @definitions = Definition
+                    .includes(:user, :word)
+                    .where(word_id: @word.id)
+                    .order(likes_counter: :desc, created_at: :desc)
+                    .page params[:page]
+    render template: 'words/show'
   end
 end
