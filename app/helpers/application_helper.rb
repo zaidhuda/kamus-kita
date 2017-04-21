6240ee176eaa1dfc175114461e4981761028f617
Zaid Huda <tripledot99@gmail.com>
2017-04-21 12:26:52 +0800

    updated meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,20 @@ module ApplicationHelper
       site: 'KamusKita',
       title: '',
       reverse: true,
+      url: request.original_url,
       description: 'Kamus sempoi untuk bahasa borak',
       image: "#{root_url}apple-icon.png",
+      fb: {
+        app_id: "689597981228619"
+      },
+      og: {
+        title:    :title,
+        type:     'website',
+        description: :description,
+        image:    "#{root_url}apple-icon.png"
+      },
       twitter: {
-        card: "summary",
+        card: 'summary',
         title: :title,
         description: :description,
         image: { _: "#{root_url}apple-icon.png" }
