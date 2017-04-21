0bed332df699677b2dd858a6c7784c369f25d95d
Zaid Huda <tripledot99@gmail.com>
2017-04-21 12:38:11 +0800

    changed default logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
       reverse: true,
       url: request.original_url,
       description: 'Kamus sempoi untuk bahasa borak',
-      image: "#{root_url}apple-icon.png",
+      image: "#{root_url}logo-small.png",
       fb: {
         app_id: "689597981228619"
       },
@@ -14,13 +14,13 @@ module ApplicationHelper
         title:    :title,
         type:     'website',
         description: :description,
-        image:    "#{root_url}apple-icon.png"
+        image:    "#{root_url}logo-small.png"
       },
       twitter: {
         card: 'summary',
         title: :title,
         description: :description,
-        image: { _: "#{root_url}apple-icon.png" }
+        image: { _: "#{root_url}logo-small.png" }
       }
     }
   end
