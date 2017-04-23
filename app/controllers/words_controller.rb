766c662f6e3c813a15a929c8cfd38c581907f99e
Zaid Huda <tripledot99@gmail.com>
2017-04-23 02:40:41 +0000

    edit word meta

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -6,16 +6,21 @@ class WordsController < ApplicationController
   def show
     begin
       @word = Word.includes(:best_definition).friendly.find(params[:id])
+
+      set_meta_tags title: @word.word,
+        canonical: word_path(params[:id]),
+        description: @word.best_definition.cleaned_definition.truncate(160)
     rescue ActiveRecord::RecordNotFound
       @word_not_found = true
       @word = Word.includes(:best_definition).offset(rand(Word.count)).first
+
+      set_meta_tags title: @word.word,
+        canonical: new_definition_path(word: params[:id]),
+        description: "There is no definition for #{@word.word}. Submit yours?"
     end
     @definitions = Definition.where(word_id: @word.id)
                     .includes(:user, :word)
                     .order(likes_counter: :desc, created_at: :desc)
                     .page params[:page]
-
-    set_meta_tags title: @word.word,
-      description: @word.best_definition.cleaned_definition.truncate(160)
   end
 end
