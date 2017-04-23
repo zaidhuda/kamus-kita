f3f1e2e1798ca438cc52ecfdd3cf705b26ab2ca0
Zaid Huda <tripledot99@gmail.com>
2017-04-23 02:47:54 +0000

    use id as word in show word action if word not found

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -8,15 +8,15 @@ class WordsController < ApplicationController
       @word = Word.includes(:best_definition).friendly.find(params[:id])
 
       set_meta_tags title: @word.word,
-        canonical: word_path(params[:id]),
+        canonical: word_url(params[:id]),
         description: @word.best_definition.cleaned_definition.truncate(160)
     rescue ActiveRecord::RecordNotFound
       @word_not_found = true
       @word = Word.includes(:best_definition).offset(rand(Word.count)).first
 
-      set_meta_tags title: @word.word,
-        canonical: new_definition_path(word: params[:id]),
-        description: "There is no definition for #{@word.word}. Submit yours?"
+      set_meta_tags title: params[:id],
+        canonical: new_definition_url(word: params[:id]),
+        description: "There is no definition for #{params[:id]}. Submit yours?"
     end
     @definitions = Definition.where(word_id: @word.id)
                     .includes(:user, :word)
