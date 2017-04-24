21b5d245731e487dc00faf1e9ab92dd0e15b7653
Zaid Huda <tripledot99@gmail.com>
2017-04-24 09:47:33 -0700

    added mailgun

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,18 +36,7 @@ Rails.application.configure do
     api_key: ENV['MAILGUN_API_KEY'],
     domain: ENV['MAILGUN_DOMAIN']
   }
-
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :user_name => 'd6d6272ea8a203',
-    :password => 'a29a4fa6318ce6',
-    :address => 'smtp.mailtrap.io',
-    :domain => 'smtp.mailtrap.io',
-    :port => '2525',
-    :authentication => :cram_md5
-  }
-
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
