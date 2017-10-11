732a060eb36a26c936f9e4458f2c7e7d6035cbb1
Zaid Huda <tripledot99@gmail.com>
2017-10-12 07:36:01 +0800

    updated faker

--- db/faker.rb
+++ db/faker.rb
@@ -10,5 +10,11 @@ end
 
 p 'Generating Definitions'
 (1..1000).each do
-  User.find(rand(1..20)).definitions.create!(original_word: Faker::Lorem.word, definition: Faker::Lorem.paragraph, example: Faker::Lorem.paragraph)
+  definition = Faker::Lorem.paragraph
+  definition = definition.insert(rand(10..definition.size-10), " [#{Faker::Lorem.word}] ")
+
+  example = Faker::Lorem.paragraph
+  example = example.insert(rand(10..example.size-10), " [#{Faker::Lorem.word}] ")
+
+  User.find(rand(1..20)).definitions.create!(original_word: Faker::Lorem.word, definition: definition, example: example)
 end
